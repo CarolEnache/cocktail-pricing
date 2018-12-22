35b6ca980b9724799a4f9722ef842d1f4cf8ff2e
Carol Enache <enache.carol@gmail.com>
2018-12-22 20:40:00 +0000

    make use of useEffect for the life cicle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
   //Declare a new state variable, which we'll call 'count'
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    //Update the document title using the browser API
+    document.title = `You  clicked ${count} times`;
+  });
 
   return (
     <div>
