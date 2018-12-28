dd493d4bba8f5f716dfa737c54de65f363f26630
Carol Enache <enache.carol@gmail.com>
2018-12-28 21:47:08 +0000

    make use of React Router and create the BOARD LIST

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { createContext, useState, useEffect, useReducer } from 'react';
+import React, {
+   createContext, 
+   useState, 
+   useEffect, 
+   useReducer,
+   useRef 
+  } from 'react';
+import { Link } from 'react-router-dom';
+import firebase from './firebase';
 import { initialState, reducer } from './redux';
 import NumberStepper from './component/counter';
-import Create from './component/create';
-import Edit from './component/edit';
-import Show from './component/show';
-import './App.css';
 
 export const StateContext = createContext(initialState);
 export const DispatchContext = createContext();
@@ -13,7 +17,30 @@ const App = () => {
   //Declare a new state variable, which we'll call 'count'
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const ref = useRef(firebase.firestore().collection('boards'));
+  const unsubscribe = useRef(null);
+  const [boards, setBoards] = useState([]);
+  
+  const onCollectionUpdate = (querySnapshot) => {
+    // console.log(querySnapshot)
+    querySnapshot.forEach((doc) => {
+      const { title, descritption, author } = doc.data();
+      //need to refactor this push() method
+      boards.push({
+        key: doc.id,
+        doc, //DocumentSnapshot
+        title,
+        descritption,
+        author,
+      });
+    });
+    setBoards(boards)
+  }
+  
   useEffect(() => {
+    console.log( )
+    unsubscribe.current = ref.current.onSnapshot(() => onCollectionUpdate)
+    console.log(unsubscribe)
     //Update the document title using the browser API
     document.title = `You  clicked ${state.number} times`;
   });
@@ -21,12 +48,39 @@ const App = () => {
   return (
     <DispatchContext.Provider value={dispatch}>
       <StateContext.Provider value={state}>
-        <div>
-          <p>You clicked {state.number} times </p>
-          <NumberStepper />
-          <Create />
-          <Edit />
-          <Show />
+        <div className='container'>
+          <div className='panel panel-default'>
+            <div className='panel-heading'>
+              <h3 className='panel-title'>
+                BOARD LIST
+              </h3>
+            </div>
+          </div> 
+          <div className='panel-body'>
+            <h4><Link to='/create'>Add Board</Link></h4>
+            <table className='table table-stipe'>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Description</th>
+                  <th>Author</th>
+                </tr>
+              </thead>
+              <tbody>
+                {boards.map(board =>
+                    <tr>
+                      <td><Link to={`/show/${board.key}`}>{board.title}</Link></td>
+                      <td>{board.description}</td>
+                      <td>{board.author}</td>
+                    </tr>
+                  )}
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <p>You clicked {state.number} times </p>
+            <NumberStepper />          
+          </div>
         </div>
       </StateContext.Provider>
     </DispatchContext.Provider>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
 import './index.css';
 import App from './App';
+import './App.css';
 import * as serviceWorker from './serviceWorker';
+import Create from './component/create';
+import Edit from './component/edit';
+import Show from './component/show';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route path='/create' component={Create} />
+            <Route path='/edit/:id' component={Edit} />
+            <Route path='/show/:id' component={Show} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
