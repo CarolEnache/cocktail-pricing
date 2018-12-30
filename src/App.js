21805fefcea1882264e40af256d1f71234e25631
Carol Enache <enache.carol@gmail.com>
2018-12-30 03:06:21 +0000

    separate concerns between app and create

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {
    createContext, 
-   useState, 
    useEffect, 
    useReducer,
   } from 'react';
@@ -15,31 +14,13 @@ export const DispatchContext = createContext();
 const App = () => {
   //Declare a new state variable, which we'll call 'count'
   const [state, dispatch] = useReducer(reducer, initialState);
-  // console.log(state)
   const db = firebase.firestore();
   db.settings({ timestampsInSnapshots: true });
-
-  const updateInput = e => {
-    dispatch({
-      type: 'BEVEREAGE_FORM_STATE', 
-      name: e.target.name, 
-      payload: e.target.value
-    })
-  }
-
-  const addBeverage = (e) => {
-    e.preventDefault()
-    db.collection('beverageList').add({
-      BeverageName: state.BeverageName,
-      BeveragePrice: state.BeveragePrice,
-      BeverageType: state.BeverageType
-    })
-    dispatch({type: 'BEVERAGE_FORM_SUBMIT'});
-  }
 
   useEffect(() => {
     //Update the document title using the browser API
     document.title = `You  clicked ${state.number} times`;
+
     db.collection('beverageList').get().then((querySnapshot) => {
       const beverageItems = []
       querySnapshot.forEach(doc => {
@@ -52,30 +33,6 @@ const App = () => {
   return (
     <DispatchContext.Provider value={dispatch}>
       <StateContext.Provider value={state}>
-        <form onSubmit={(e) => addBeverage(e)}>
-          <input 
-            type='text'
-            name='BeverageName'
-            placeholder='Beverage Name'
-            onChange={(e) => updateInput(e)}
-            value={state.BeverageName}
-            />
-          <input 
-            type='number'
-            name='BeveragePrice'
-            placeholder='Beverage Price'
-            onChange={(e) => updateInput(e)}
-            value={state.BeveragePrice}
-            />
-          <input 
-            type='text'
-            name='BeverageType'
-            placeholder='Beverage Type'
-            onChange={(e) => updateInput(e)}
-            value={state.BeverageType}
-            />
-          <button type="submit">Submit</button>
-        </form>
         <div>
           <p>You clicked {state.number} times </p>
           <NumberStepper />
@@ -83,7 +40,7 @@ const App = () => {
         {console.log(state.BeverageList)}
         {state.BeverageList.map(beverageItem => {
           return(
-            <li>{beverageItem.BeverageName} {beverageItem.BeverageType}  £ {beverageItem.BeveragePrice}</li>
+            <li>{beverageItem.BeverageName} {beverageItem.BeverageType} £ {beverageItem.BeveragePrice}</li>
           )
         })}
       </StateContext.Provider>
--- src/component/create/Create.component.js
+++ src/component/create/Create.component.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import firebase from '../../firebase';
+import { initialState, reducer } from '../../redux';
 
 const Create = () => {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const db = firebase.firestore();
+
+    const updateInput = e => {
+        dispatch({
+            type: 'BEVEREAGE_FORM_STATE',
+            name: e.target.name,
+            payload: e.target.value
+        })
+    }
+
+    const addBeverage = (e) => {
+        e.preventDefault()
+        db.collection('beverageList').add({
+            BeverageName: state.BeverageName,
+            BeveragePrice: state.BeveragePrice,
+            BeverageType: state.BeverageType
+        })
+        dispatch({ type: 'BEVERAGE_FORM_SUBMIT' });
+    }
+
     return (
         <div>
             <h1>hello from the Create component</h1>
+            <form onSubmit={(e) => addBeverage(e)}>
+                <input
+                    type='text'
+                    name='BeverageName'
+                    placeholder='Beverage Name'
+                    onChange={(e) => updateInput(e)}
+                    value={state.BeverageName}
+                />
+                <input
+                    type='number'
+                    name='BeveragePrice'
+                    placeholder='Beverage Price'
+                    onChange={(e) => updateInput(e)}
+                    value={state.BeveragePrice}
+                />
+                <input
+                    type='text'
+                    name='BeverageType'
+                    placeholder='Beverage Type'
+                    onChange={(e) => updateInput(e)}
+                    value={state.BeverageType}
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
