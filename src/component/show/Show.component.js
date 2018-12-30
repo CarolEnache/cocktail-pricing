ffe5c8ba918c39453fa0d35caf3ec98232037e1f
Carol Enache <enache.carol@gmail.com>
2018-12-30 17:01:59 +0000

    Make it posible to delete items

--- src/component/show/Show.component.js
+++ src/component/show/Show.component.js
@@ -6,29 +6,35 @@ const Show = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
     const db = firebase.firestore();
     db.settings({ timestampsInSnapshots: true });
-
-    useEffect(() => {
-        //Update the document title using the browser API
-        // document.title = `You  clicked ${state.number} times`;
-
-        db.collection('beverageList').get().then((querySnapshot) => {
-            const beverageItems = []
-            querySnapshot.forEach(doc => {
-                beverageItems.push(Object.assign({ id: doc.id, ...doc.data() }))
-            })
-            dispatch({ type: 'BEVERAGE_LIST', payload: beverageItems })
+    const ref = db.collection('beverageList');
+    const getData = () => ref.get().then((querySnapshot) => {
+        const beverageItems = []
+        querySnapshot.forEach(doc => {
+            beverageItems.push(Object.assign({ id: doc.id, ...doc.data() }))
         })
-    }, { db });
+        dispatch({ type: 'BEVERAGE_LIST', payload: beverageItems })
+    })
+
+    useEffect(() => {getData()}, { db });
+
+    const showId = (id) => {
+        ref.doc(id).delete().then(() => {
+            console.log('Document succeffully deleted');
+        }).catch((error) => {
+            console.error('Error removing document: ', error)
+        })
+        getData();
+    }
 
     return (
         <div>
             <h1>Hello from the Show Component</h1>
-            {console.log(state)}
-            {console.log(state.BeverageList)}
-
             {state.BeverageList.map(beverageItem => {
                 return (
-                    <li>{beverageItem.BeverageName} {beverageItem.BeverageType} £ {beverageItem.BeveragePrice}</li>
+                    <div>
+                        <li>{beverageItem.BeverageName} {beverageItem.BeverageType} £ {beverageItem.BeveragePrice}</li>
+                        <button onClick={() => showId(beverageItem.id)}>Delete</button>
+                    </div>
                 )
             })}
         </div>
